2c8bdff0c320e54a660cdde773dbfd64ab8956fd
Zelendoren <blockchain@junkineering.io>
2023-01-23 21:27:23 +0200

    fix: fixes

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -7,53 +7,64 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "hardhat/console.sol";
 
 contract StakingRewards is Pausable, Ownable, ReentrancyGuard {
-
     IERC20 myToken;
 
     // 30 Days (30 * 24 * 60 * 60)
-    uint256 public planDuration = 2592000;//2592000
+    uint256 public planDuration = 2592000; //2592000
 
     // 180 Days (180 * 24 * 60 * 60)
     uint256 _planExpired = 15552000;
 
-    uint8 public interestRate = 32;
+    uint8 public interestRate = 32; //?
     uint256 public planExpired;
     uint8 public totalStakers;
 
-    struct StakeInfo {        
+    struct StakeInfo {
         uint256 startTS;
-        uint256 endTS;        
-        uint256 amount; 
-        uint256 claimed;       
+        uint256 endTS;
+        uint256 amount;
+        uint256 claimed;
     }
-    
+
     event Staked(address indexed from, uint256 amount);
     event Claimed(address indexed from, uint256 amount);
-    
+
     mapping(address => StakeInfo) public stakeInfos;
     mapping(address => bool) public addressStaked;
 
-
-    constructor(IERC20 _tokenAddress) { 
-        require(address(_tokenAddress) != address(0),"Token Address cannot be address 0");                
-        myToken = _tokenAddress;        
-        planExpired = block.timestamp + _planExpired;
+    constructor(IERC20 _tokenAddress) {
+        require(
+            address(_tokenAddress) != address(0),
+            "Token Address cannot be address 0"
+        );
+        myToken = _tokenAddress;
+        planExpired = block.timestamp + _planExpired; //????
         totalStakers = 0;
-    }    
-
-    function transferToken(address to,uint256 amount) external onlyOwner{
-        require(myToken.transfer(to, amount), "Token transfer failed!");  
     }
 
-    function claimReward() external returns (bool){
-        require(addressStaked[_msgSender()] == true, "You are not participated");
-        require(stakeInfos[_msgSender()].endTS < block.timestamp, "Stake Time is not over yet");
-        require(stakeInfos[_msgSender()].claimed == 0, "Already claimed");
+    ///???
+    function transferToken(address to, uint256 amount) external onlyOwner {
+        require(myToken.transfer(to, amount), "Token transfer failed!");
+    }
+
+    // msg.sender ?
+    function claimReward() external returns (bool) {
+        require(
+            addressStaked[_msgSender()] == true,
+            "You are not participated"
+        );
+        require(
+            stakeInfos[_msgSender()].endTS < block.timestamp,
+            "Stake Time is not over yet"
+        );
+        require(stakeInfos[_msgSender()].claimed == 0, "Already claimed"); //?
 
         uint256 stakeAmount = stakeInfos[_msgSender()].amount;
-        uint256 totalTokens = stakeAmount + (stakeAmount * interestRate / 100);
+        uint256 totalTokens = stakeAmount +
+            ((stakeAmount * interestRate) / 100);
         stakeInfos[_msgSender()].claimed == totalTokens;
         myToken.transfer(_msgSender(), totalTokens);
 
@@ -63,35 +74,51 @@ contract StakingRewards is Pausable, Ownable, ReentrancyGuard {
     }
 
     function getTokenExpiry() external view returns (uint256) {
-        require(addressStaked[_msgSender()] == true, "You are not participated");
+        require(
+            addressStaked[_msgSender()] == true,
+            "You are not participated"
+        ); //?
+
         return stakeInfos[_msgSender()].endTS;
     }
 
     function getTokenAmountStaked() external view returns (uint256) {
-        require(addressStaked[_msgSender()] == true, "You are not participated");
+        require(
+            addressStaked[_msgSender()] == true,
+            "You are not participated"
+        ); //?
+
         return stakeInfos[_msgSender()].amount;
     }
 
     function stakeToken(uint256 stakeAmount) external payable whenNotPaused {
-        require(stakeAmount >0, "Stake amount should be correct");
-        require(block.timestamp < planExpired , "Plan Expired");
-        require(addressStaked[_msgSender()] == false, "You already participated");
-        require(myToken.balanceOf(_msgSender()) >= stakeAmount, "Insufficient Balance");
-        
-           myToken.transferFrom(_msgSender(), address(this), stakeAmount);
-            totalStakers++;
-            addressStaked[_msgSender()] = true;
+        require(stakeAmount > 0, "Stake amount should be correct");
+        require(block.timestamp < planExpired, "Plan Expired");
+        console.log(addressStaked[msg.sender]);
+        console.log(address(msg.sender));
+        require(
+            addressStaked[msg.sender] == false,
+            "You already participated 1"
+        );
+        //check approve
+        require(
+            myToken.balanceOf(_msgSender()) >= stakeAmount,
+            "Insufficient Balance"
+        );
 
-            stakeInfos[_msgSender()] = StakeInfo({                
-                startTS: block.timestamp,
-                endTS: block.timestamp + planDuration,
-                amount: stakeAmount,
-                claimed: 0
-            });
-        
+        myToken.transferFrom(_msgSender(), address(this), stakeAmount);
+        totalStakers++;
+        addressStaked[_msgSender()] = true;
+
+        stakeInfos[_msgSender()] = StakeInfo({
+            startTS: block.timestamp,
+            endTS: block.timestamp + planDuration,
+            amount: stakeAmount,
+            claimed: 0
+        });
+
         emit Staked(_msgSender(), stakeAmount);
-    }    
-
+    }
 
     function pause() external onlyOwner {
         _pause();
--- test/StakingRewards.js
+++ test/StakingRewards.js
@@ -1,92 +1,90 @@
 const { expect } = require("chai");
 
-
 describe("Contract Version 1 test", function () {
+  let token;
+  let owner;
+  let user1;
+  let user2;
+  let owner_balance;
+  let stake_amount = 100;
 
-    let token;
-    let owner;
-    let user1;
-    let owner_balance;
-    let stake_amount = 100;
+  before(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("MyToken");
+    token = await Token.deploy("TKN", "T");
+    await token.deployed();
 
-    before(async function () {
-        [owner, user1] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory("MyToken");
-        token = await Token.deploy('TKN', 'T');
-        await token.deployed();
+    const StakingRewards = await ethers.getContractFactory("StakingRewards");
+    stackingToken = await StakingRewards.deploy(token.address); //
+    await stackingToken.deployed();
 
-        const StakingRewards = await ethers.getContractFactory("StakingRewards");
-        stackingToken = await StakingRewards.deploy(token.address);//
-        await stackingToken.deployed();
+    await token.mint(owner.address, 1000);
+  });
+  it("Should staking 100 tokens", async () => {
+    owner_balance = await token.balanceOf(owner.address);
+    console.log(owner_balance);
 
-        await token.mint(owner.address, 1000);
-    });
-    it("Should staking 100 tokens", async () => {
+    // await token.transfer(stackingToken.address, stake_amount);//?
+    await token.approve(stackingToken.address, stake_amount);
+    let tx = await stackingToken.stakeToken(stake_amount);
 
-        owner_balance = await token.balanceOf(owner.address);
-        console.log(owner_balance);
+    console.log(await token.balanceOf(owner.address));
+    console.log(await stackingToken.getTokenAmountStaked());
 
-        // await token.transfer(stackingToken.address, stake_amount);//?
-        await token.approve(stackingToken.address, stake_amount);
-        let tx = await stackingToken.stakeToken(stake_amount);
-
-        console.log(await token.balanceOf(owner.address));
-        console.log(await stackingToken.getTokenAmountStaked());
-        
-        await expect(() => tx).to.changeTokenBalance(
-            token, owner.address, -stake_amount
-          );
-    });
-    // assert.equal(error.reason, "DevToken: Cannot stake more than you own");
-    it("Should getTokenAmountStaked", async () => {
-
-        let tx = await stackingToken.getTokenAmountStaked();
-        await expect(tx).to.equal(stake_amount);
-    });
-    it("Should be reverted with `You are not participated`", async () => {
-        /* await token.approve(stackingToken.address, stake_amount);
+    await expect(() => tx).to.changeTokenBalance(
+      token,
+      owner.address,
+      -stake_amount
+    );
+  });
+  // assert.equal(error.reason, "DevToken: Cannot stake more than you own");
+  it("Should getTokenAmountStaked", async () => {
+    let tx = await stackingToken.getTokenAmountStaked();
+    await expect(tx).to.equal(stake_amount);
+  });
+  it("Should be reverted with `You are not participated`", async () => {
+    /* await token.approve(stackingToken.address, stake_amount);
         let tx = await stackingToken.connect(user1).getTokenAmountStaked();
         await expect(()=>tx).to.be.revertedWith(
             "You are not participated"
         );*/
-    });
-    it("Should be reverted with `You already participated`", async () => {
-
-        // await token.approve(stackingToken.address, stake_amount);
-        // let tx = await stackingToken.stakeToken(stake_amount);
-        // await expect(()=>tx).to.be.revertedWith(
-        //     "You already participated"
-        // );
-    });
-    it("Should be reverted with `Stake Time is not over yet`", async () => {
-
-        // let tx = await stackingToken.claimReward();
-        // await expect(()=>tx).to.be.revertedWith(
-        //     "Stake Time is not over yet"
-        // );
-    });
-    it("Should be reverted with `Stake Time is not over yet`", async () => {
-
-        // let tx = await setTimeout(await stackingToken.claimReward(), 10000);
-        // await expect(()=>tx).to.be.revertedWith(
-        //     "Stake Time is not over yet"
-        // );
-    });
-    it("I want to connecting other user and staked from it, but it `You already participated`", async () => {
-
-        // await token.mint(user1.address, 2000);
-        // owner_balance = await token.balanceOf(user1.address);
-        // console.log(owner_balance);
-
-        // await token.connect(user1).approve(stackingToken.address, stake_amount);
-        // let tx = await stackingToken.stakeToken(stake_amount);
-
-        // console.log(await token.balanceOf(user1.address));
-        // console.log(await stackingToken.getTokenAmountStaked());
-        
-        // await expect(() => tx).to.changeTokenBalance(
-        //     token, user1.address, -stake_amount
-        //   );
-
-    });
+  });
+  it("Should be reverted with `You already participated`", async () => {
+    // await token.approve(stackingToken.address, stake_amount);
+    // let tx = await stackingToken.stakeToken(stake_amount);
+    // await expect(()=>tx).to.be.revertedWith(
+    //     "You already participated"
+    // );
+  });
+  it("Should be reverted with `Stake Time is not over yet`", async () => {
+    // let tx = await stackingToken.claimReward();
+    // await expect(()=>tx).to.be.revertedWith(
+    //     "Stake Time is not over yet"
+    // );
+  });
+  it("Should be reverted with `Stake Time is not over yet`", async () => {
+    // let tx = await setTimeout(await stackingToken.claimReward(), 10000);
+    // await expect(()=>tx).to.be.revertedWith(
+    //     "Stake Time is not over yet"
+    // );
+  });
+  it("I want to connecting other user and staked from it, but it `You already participated`", async () => {
+    await token.mint(user2.address, 2000);
+    owner_balance = await token.balanceOf(user2.address);
+    console.log(owner_balance);
+    let tx = await token
+      .connect(user2)
+      .approve(stackingToken.address, stake_amount);
+    await tx.wait();
+    // tx = ;
+    // await tx.wait();
+    console.log(await token.balanceOf(user2.address));
+    console.log(await stackingToken.addressStaked(user2.address));
+    console.log(user1.address);
+    console.log(user2.address);
+    console.log(owner.address);
+    await expect(
+      stackingToken.connect(user2).stakeToken(stake_amount)
+    ).to.changeTokenBalance(token, user2.address, -stake_amount);
+  });
 });
